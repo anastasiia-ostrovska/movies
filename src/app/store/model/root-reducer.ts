7019b978a216fed257e743798c5ec04ea2a3a22d
anastasiia-ostrovska <anastasiyakov99@gmail.com>
2025-06-17 17:58:21 +0300

    entity-movie: connected moviesParamsSlice to root-reducer

--- src/app/store/model/root-reducer.ts
+++ src/app/store/model/root-reducer.ts
@@ -1,5 +1,6 @@
 import { combineSlices } from '@reduxjs/toolkit';
 import { baseAPI } from '@/shared/api';
 import { sessionSlice } from '@/entities/session';
+import { moviesParamsSlice } from '@/entities/movie';
 
-export const rootReducer = combineSlices(baseAPI, sessionSlice);
+export const rootReducer = combineSlices(baseAPI, sessionSlice, moviesParamsSlice);

9ecd4bb74177efbf11498beb0f3c96484c0bc94f
Anastasiia Ostrovska <95317301+anastasiia-ostrovska@users.noreply.github.com>
2025-06-17 10:58:37 +0300

    Entity : session slice  (#6)
    * local-storage: created reusable localStorage handlers in shared/lib; created token-storage-key constant; created token-local-storage-handlers
    
    * session-slice: created session-slice to store jwt-token; added to root-reducer
    
    * api-auth-header: set token to authorization headers from state in baseQuery

--- src/shared/api/base-api.ts
+++ src/shared/api/base-api.ts
@@ -1,10 +1,10 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import type { RootState } from '@/app/store';
 
 const baseQuery = fetchBaseQuery({
   baseUrl: import.meta.env.VITE_API_URL,
-  prepareHeaders: headers => {
-    // template before session entity implemented
-    const token = '';
+  prepareHeaders: (headers, { getState }) => {
+    const { token } = (getState() as RootState).session;
 
     if (token) {
       headers.set('authorization', `Bearer ${token}`);

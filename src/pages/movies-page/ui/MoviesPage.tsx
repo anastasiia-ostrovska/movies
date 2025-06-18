70b70a91d8ea81f8904321c3349c92c583788bd4
anastasiia-ostrovska <anastasiyakov99@gmail.com>
2025-06-18 21:41:59 +0300

    movies-pagination: add movies-pagination

--- src/pages/movies-page/ui/MoviesPage.tsx
+++ src/pages/movies-page/ui/MoviesPage.tsx
@@ -1,22 +1,19 @@
 import Stack from '@mui/material/Stack';
-import { useModalController } from '@/entities/modal';
-import Button from '@mui/material/Button';
 import { ModalController } from '@/app/modal-controller';
 import { MovieCardList } from '@/widgets/movie-card-list';
 import { SortMoviesMenu } from '@/features/sort-movies-menu';
+import { AddMoviesButton } from '@/features/add-movies-button';
 
 const MoviesPage = () => {
-  const { showModal } = useModalController();
-
-  const handleOpenModal = () => {
-    showModal({ modalVariant: 'addMovies', id: 1 });
-  };
-
   return (
-    <Stack spacing={4} alignItems="center" p={{ xs: 2, sm: 4, md: 8 }}>
-      <SortMoviesMenu />
-      <Button onClick={handleOpenModal}>Show Add Movies</Button>
-      <MovieCardList />
+    <Stack spacing={4} alignItems="center" p={{ xs: 2, sm: 4, md: 4 }}>
+      <Stack sx={{ width: '100%', maxWidth: 900, m: 0, p: 0 }}>
+        <Stack sx={{ alignSelf: 'end', mb: 1 }}>
+          <SortMoviesMenu />
+        </Stack>
+        <MovieCardList />
+      </Stack>
+      <AddMoviesButton />
       <ModalController />
     </Stack>
   );

9394816f7230c7167cf9c478b2a25a9941fb9335
anastasiia-ostrovska <anastasiyakov99@gmail.com>
2025-06-18 15:48:42 +0300

    movie-card: create generateFakeMoviesList util function and use-movies-params hook to receive data

--- src/entities/movie/index.ts
+++ src/entities/movie/index.ts
@@ -3,3 +3,5 @@ export * from './model/movies-params-slice';
 export * from './api/movie-api';
 
 export { moviesParamsSlice } from './model/movies-params-slice';
+export { generateFakeMoviesList } from './lib/generateFakeMoviesList';
+export { useMoviesParams } from './model/use-movies-params';
